dad32c4b0f3bbb79530e3b86b6b353106212fdd8
KEINOS <github@keinos.com>
2019-06-14 02:24:21 +0900

    chore: display process status

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-which dockle 2>&1 > /dev/null
+which docker 2>&1 > /dev/null
 if [ $? -ne 0 ]; then
-   echo '- Dockle not found. Please install dockle before use.'
-   exit 1
+    echo '- Docker not found. Please install Docker before use.'
+    exit 1
 fi
 
+which dockle 2>&1 > /dev/null
+if [ $? -ne 0 ]; then
+    echo '- Dockle not found. Please install Dockle before use.'
+    exit 1
+fi
+
+echo '- Building: Docker image ...'
 export DOCKER_CONTENT_TRUST=1 && \
 docker build --no-cache -t my_image:my_test . && \
+echo '- Auditing Security: Docker image ...' && \
 dockle --debug --clear-cache my_image:my_test
